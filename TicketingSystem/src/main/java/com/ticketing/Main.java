38b60fbeaefcbb65fe655b385182011bf8eb3ef0
mostafa <mostafa.osman.fathi@gmail.com>
2025-04-01 02:28:45 +0200

    adding build Artifact for jar

--- TicketingSystem/src/main/java/com/ticketing/Main.java
+++ TicketingSystem/src/main/java/com/ticketing/Main.java
@@ -1,7 +1,11 @@
 package com.ticketing;
 
+import com.ticketing.view.DatabaseSelectionPage;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        SwingUtilities.invokeLater(DatabaseSelectionPage::new);
     }
 }
